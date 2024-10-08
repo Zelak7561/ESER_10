6c189d16d20ce2dbd08069283da5dac826f289de
Mario21145 <mariomazziotti756@gmail.com>
2024-10-08 20:07:02 +0200

    commit 1

---
+++ cmake-build-debug/CMakeFiles/untitled5.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled5.dir/main.c.obj"
+  "CMakeFiles/untitled5.dir/main.c.obj.d"
+  "libuntitled5.dll.a"
+  "untitled5.exe"
+  "untitled5.exe.manifest"
+  "untitled5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/untitled5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
