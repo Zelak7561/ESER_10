601b7ba46ce0bc372640bf3073062c16db46dbe6
Mario21145 <mariomazziotti756@gmail.com>
2024-10-08 20:21:56 +0200

    commit 2
    Added the counter of the tentativi correnti

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main(void) {
-
-    const int tentativi = 10;
+    int tentativi = 10;
+    int tentativiCorrenti = 10;
     int risposta;
 
     printf("**************************************************\n");
@@ -11,24 +11,36 @@ int main(void) {
     printf("\n");
 
     printf("**************************************************\n");
-    printf("Numero tentativi a disposizione: [%d]\n");
+    printf("Numero tentativi totali a disposizione: [%d]\n", tentativi);
     printf("**************************************************\n");
+
+    printf("\n");
 
     for (int i = 0; i < tentativi; i++) {
         risposta = 0;
         printf("Inserire risposta:\n");
         scanf("%d", &risposta);
-        if(risposta == 1939) {
+
+
+        if (risposta == 1939) {
             printf("Risposta esatta\n");
             break;
         }
 
-        if(risposta != 1939) {
+        if (risposta != 1939) {
+            tentativiCorrenti--;
             printf("Risposta sbagliata riprovare\n");
+            printf("\n");
+            printf("**************************************************\n");
+            printf("Tentavi correnti: %d \n", tentativiCorrenti);
+            printf("**************************************************\n");
         }
+
+        if (tentativiCorrenti == 0) {
+            printf("Tentativi esauriti\n");
+        }
+
+
     }
-
-
-
     return 0;
 }
